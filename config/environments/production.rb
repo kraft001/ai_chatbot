0fdb87bfc6835738ab1245c8174f727ae43504d3
Konstantin Rafalskii <kraft001@gmail.com>
2023-08-05 17:47:46 +0300

    Adapt config to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,8 +42,11 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.url = "ws://ai-chatbot-23-78433f90e38a.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [
+    'https://ai-chatbot-23-78433f90e38a.herokuapp.com/',
+    /https:\/\/ai-chatbot-23-78433f90e38a.herokuapp.com.*/
+  ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -67,6 +70,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.default_url_options = { host: 'ai-chatbot-23-78433f90e38a.herokuapp.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
